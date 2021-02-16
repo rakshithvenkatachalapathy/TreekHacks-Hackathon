32a52dcbac5ee3153ee4e027b5bd81d223103853
rakshith <rvenkatachalapathy@dons.usfca.edu>
2021-02-16 11:29:50 -0800

    all files

---
+++ firestore/app.js
@@ -0,0 +1,11 @@
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+var config = {
+    apiKey: "API_KEY",
+    authDomain: "PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://PROJECT_ID.firebaseio.com",
+    projectId: "PROJECT_ID",
+    storageBucket: "PROJECT_ID.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+    measurementId: "G-MEASUREMENT_ID",
+  };
